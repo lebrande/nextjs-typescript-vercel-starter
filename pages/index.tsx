372acc9c3cf9e6788b516f0abd4e1342e17d6e51
Jakub <jakub.pusiak@gmail.com>
2021-07-07 21:27:36 +0200

    JS to TS.

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>JS Dżem</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          JS Dżem
+        </h1>
+      </main>
+    </div>
+  )
+}
